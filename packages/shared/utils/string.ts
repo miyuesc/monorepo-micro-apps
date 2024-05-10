2d8ea23461d6d6cb5094fb619fec53a81350eec9
MiyueSC <913784771@qq.com>
2024-05-10 17:12:00 +0800

    shared utils

---
+++ packages/shared/utils/string.ts
@@ -0,0 +1,52 @@
+/**
+ * 在指定位置插入字符串
+ *
+ * insertString('hello world', 6, 'my ') -> 'hello my world'
+ */
+export function insertString(str: string, index: number, insertStr: string): string {
+  return str.substring(0, index) + insertStr + str.substring(index)
+}
+
+/**
+ * 首字母大写
+ *
+ * capitalize('hello')   -> 'Hello'
+ */
+export function capitalize(str: string): string {
+  if (!str || str.length === 0)
+    return ''
+  const lower = str.toLowerCase()
+  return lower.substring(0, 1).toUpperCase() + lower.substring(1, lower.length)
+}
+
+/**
+ * 首字母大写
+ *
+ * pascal('hello world')   -> 'HelloWorld'
+ */
+export function pascal(str: string): string {
+  const parts = str?.split(/[.\-\s_]/).map(x => x.toLowerCase()) ?? []
+  if (parts.length === 0)
+    return ''
+  return parts.map(str => str.charAt(0).toUpperCase() + str.slice(1)).join('')
+}
+
+/**
+ * 转驼峰命名
+ *
+ * camel('hello world')   -> 'helloWorld'
+ */
+export function camel(str: string): string {
+  const parts
+    = str
+      ?.replace(/([A-Z])+/g, capitalize)
+      ?.split(/(?=[A-Z])|[.\-\s_]/)
+      .map(x => x.toLowerCase()) ?? []
+  if (parts.length === 0)
+    return ''
+  if (parts.length === 1)
+    return parts[0]
+  return parts.reduce((acc, part) => {
+    return `${acc}${part.charAt(0).toUpperCase()}${part.slice(1)}`
+  })
+}
