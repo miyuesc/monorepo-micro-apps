f267f644c7992dd4818bdd5227fe478d6fa4656e
MiyueSC <913784771@qq.com>
2024-05-14 13:53:42 +0800

    shared utils - linked list

---
+++ packages/shared/structure/linked-list.ts
@@ -0,0 +1,77 @@
+export interface LinkedListNode {
+  value: unknown
+  prev?: LinkedListNode
+  next?: LinkedListNode
+}
+
+export interface LinkedListConfig {
+  maxLength: number
+}
+
+export type InsertPosition = 'before' | 'after'
+
+class LinkedList {
+  _config: LinkedListConfig
+  _list: LinkedListNode[]
+
+  constructor(config: LinkedListConfig) {
+    this._config = config
+    this._list = []
+  }
+
+  clear() {
+    this._list = []
+    return this
+  }
+
+  size() {
+    return this._list.length
+  }
+
+  isEmpty() {
+    return this.size() === 0
+  }
+
+  _createListNode(value: unknown): LinkedListNode {
+    return { value }
+  }
+
+  append(value: unknown) {
+    const node = this._createListNode(value)
+    if (this.size()) {
+      const prev = this._list[this.size() - 1]
+      prev.next = node
+      node.prev = prev
+    }
+    this._list.push(node)
+    return node
+  }
+
+  prepend(value: unknown) {
+    const node = this._createListNode(value)
+    if (this.size()) {
+      const head = this._list[0]
+      head.prev = node
+      node.next = head
+    }
+    this._list.unshift(node)
+    return node
+  }
+
+  insert(value: unknown, anchor: LinkedListNode, position: InsertPosition = 'before') {
+    const node = this._createListNode(value)
+    if (position === 'before') {
+      node.prev = anchor.prev
+      node.next = anchor
+      anchor.prev = node
+    }
+    else {
+      node.next = anchor.next
+      node.prev = anchor
+      anchor.next = node
+    }
+    return node
+  }
+}
+
+export default LinkedList
