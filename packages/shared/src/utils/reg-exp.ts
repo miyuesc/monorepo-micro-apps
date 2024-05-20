9f3be417bce973d49485527a9612658a6271646d
MiyueSC <913784771@qq.com>
2024-05-20 09:53:52 +0800

    package - shared reg

--- packages/shared/src/utils/reg-exp.ts
+++ packages/shared/src/utils/reg-exp.ts
@@ -99,9 +99,45 @@ export function isComplexString(str: string): boolean {
   return reg.test(str)
 }
 
-// 完整日期格式
+// 完整日期格式(YYYY-MM-DD)
 export function isFullDate(str: string): boolean {
   const reg = /^(([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]|[0-9][1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8]))))|((([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))-02-29)$/
+  return reg.test(str)
+}
+// 标准 12 小时时间格式(hh:mm:ss)
+export function isTwelveTime(str: string): boolean {
+  const reg = /^(?:1[0-2]|0?[1-9]):[0-5]\d:[0-5]\d$/
+  return reg.test(str)
+}
+// 标准 24 小时时间格式(HH:mm:ss)
+export function isTwentyFourTime(str: string): boolean {
+  const reg = /^(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d$/
+  return reg.test(str)
+}
+
+// 严格语义化版本号
+export function isStrictSemanticVersion(str: string): boolean {
+  const reg = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/
+  return reg.test(str)
+}
+// 语义化版本号
+export function isSemanticVersion(str: string, strict?: boolean): boolean {
+  if (strict)
+    return isStrictSemanticVersion(str)
+
+  const reg = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)$/
+  return reg.test(str)
+}
+
+// 标准化货币格式(必须包含分隔符，支持复数与小数)
+export function isStandardCurrency(str: string): boolean {
+  const reg = /^-?\d{1,3}(,\d{3})*(\.\d{1,2})?$/
+  return reg.test(str)
+}
+
+// 二代身份证格式
+export function isIdCard(str: string): boolean {
+  const reg = /^[1-9]\d{5}(?:18|19|20)\d{2}(?:0[1-9]|10|11|12)(?:0[1-9]|[1-2]\d|30|31)\d{3}[\dXx]$/
   return reg.test(str)
 }
 
