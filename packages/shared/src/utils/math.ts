7ea7c6a158a7a2b712a65404deb7f8bd86a07217
MiyueSC <913784771@qq.com>
2024-05-28 16:39:44 +0800

    package - shared math

---
+++ packages/shared/src/utils/math.ts
@@ -0,0 +1,34 @@
+export interface IPoint {
+  x: number
+  y: number
+}
+
+export function isPointInConvexPolygon(polygon: IPoint[], point: IPoint) {
+  let dir: number | undefined
+  for (let i = 0; i < polygon.length; i++) {
+    const start = polygon[i]
+    const end = polygon[(i + 1) % polygon.length]
+    const a = {
+      x: end.x - start.x,
+      y: end.y - start.y,
+    }
+    const b = {
+      x: point.x - start.x,
+      y: point.y - start.y,
+    }
+    // 通过叉积公式得到点在边的方向
+    const currDir = Math.sign(a.x * b.y - a.y * b.x)
+    // 点在边上，跳过
+    if (currDir === 0)
+      continue
+
+    if (dir === undefined)
+      dir = currDir
+
+    // 发现有方向不一样了，点不在凸多边形上
+    else if (dir !== currDir)
+      return false
+  }
+  // 点都在边的同一方向上
+  return true
+}
